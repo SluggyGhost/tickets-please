30aa2d3b413c53a10f1ac01a86a9936a3ac16122
Joshua <jodacost@ucsc.edu>
2025-11-06 13:55:47 -0800

    Added a "How to Play" screen

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/node_2d.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/how_to_play.tscn")
